c00650e06990c334f100ed320d2e56bf727c79e7
wjxor <shko940575@gmail.com>
2021-05-06 16:50:56 +0900

    2차원 배열에서 좌표 입력받으면 해당되는 수 출력

--- src/Op.java
+++ src/Op.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Op {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		
 		int i, j, num;
-		num = sc.nextInt();
+		i = sc.nextInt();
+		j = sc.nextInt();
 		
-		i = (num - 1) / 4;
-		j = (num - 1) % 4;
+		num = i * 4 + j + 1;
 		
-		System.out.print("(" + i + "," + j + ")");
+		System.out.print(num);
 	}
 }
