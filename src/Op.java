eea48cf781b39871bd5905f63f0994ed780b835e
wjxor <shko940575@gmail.com>
2021-05-06 16:10:56 +0900

    2차원 배열 좌표 찾기

--- src/Op.java
+++ src/Op.java
@@ -4,18 +4,13 @@ public class Op {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] a = new int[4];
-		int sum = 0;
 		
-		for (int i = 0; i < a.length; i++) {
-			a[i] = sc.nextInt();
-			
-		}
+		int i, j, num;
+		num = sc.nextInt();
 		
-		for (int j = 0; j < a.length; j++) {
-			sum += a[j];
-		}
+		i = (num - 1) / 4;
+		j = (num - 1) % 4;
 		
-		System.out.println(sum);
+		System.out.print("(" + i + "," + j + ")");
 	}
 }
