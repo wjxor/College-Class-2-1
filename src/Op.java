64a97ceb4568e2ab0305d9efae72634c4f056b77
wjxor <shko940575@gmail.com>
2021-05-06 15:12:07 +0900

    경우에 따른 사칙연산

--- src/Op.java
+++ src/Op.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class Op {
 
 	public static void main(String[] args) {
-		
-		int num;
-		
 		Scanner sc = new Scanner(System.in);
-
-		// next()로 문자열을 입력받아서 charAt(0) 0인 곳부터 데려와라!
-		char a = sc.next().charAt(0);
-	
-		num = (int) a;
+		int[] a = new int[4];
+		int sum = 0;
 		
-		System.out.printf("%d", num);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+			
+		}
 		
-		sc.close();
+		for (int j = 0; j < a.length; j++) {
+			sum += a[j];
+		}
 		
+		System.out.println(sum);
 	}
-
 }
