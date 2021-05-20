40a509b79c506d63e62a15c88b59902235777284
wjxor <shko940575@gmail.com>
2021-05-20 15:42:10 +0900

    FileInputStream을 사용해 txt파일의 글을 가져왔지만 한글이 깨진다

--- src/Op.java
+++ src/Op.java
@@ -1,31 +1,17 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.FileInputStream;
 
 public class Op {
 
-	public static void main(String[] args) {
-		String userName = "hanbit";
-		String input = "";
-		int key;
+	public static void main(String[] args) throws Exception {
+		FileInputStream fis = new FileInputStream("C:/temp/a.txt");
 		
-		try {
-			System.out.print("이름 ==> ");
-			
-			while ((key = System.in.read()) != 13) {
-				input += Character.toString((char)key);
-			}
-			
-			if (userName.equals(input)) {
-				System.out.println(input + "님 어서오세요 ~~");
-			}
-			
-			else
-				System.out.println(input + "님 등록이 안되었습니다 ~~");
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+		int ch;
+		
+		while ((ch = fis.read()) != -1) {
+			System.out.print((char) ch);
 		}
+		
+		fis.close();
 		
 	}
 }
